24a32b083d1c4e37af001a57d78431aef121413d
Attislpk <1065392882@qq.com>
2020-12-14 11:26:01 +0800

    finish A on dev

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
+        System.out.println("Hello git!1111");
         System.out.println("Hello world");
     }
 }
